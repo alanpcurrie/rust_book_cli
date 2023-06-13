49560fc7171d6b2956d301ebdb36a6ee381af24a
Alan P Currie <alanc909@outlook.com>
2023-06-13 11:28:22 +0100

    working mini grep

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+use std::fs;
+// cargo run -- searchstring example-filename.txt
+// output args = [
+//  "target/debug/rust_book_cli",
+// "searchstring",
+//  "example-filename.txt",
+// ]
 fn main() {
-    println!("Hello, world!");
+    let msg = "Should have been able to read the file";
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+    let contents = fs::read_to_string(file_path).expect(msg);
+    // Prints and returns the value of a given expression for quick and dirty debugging.
+    dbg!(query, file_path);
+    println!("searching for {}", query);
+    println!("in file {}", file_path);
+    println!("With text:\n{contents}");
 }
