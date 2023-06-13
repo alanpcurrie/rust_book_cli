a8fe517ae10294dd88a265047b05fdefc47688ae
Alan P Currie <alanc909@outlook.com>
2023-06-13 14:45:49 +0100

    refactor to library

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 use std::env;
-use std::fs;
-// cargo run -- searchstring example-filename.txt
-// output args = [
-//  "target/debug/rust_book_cli",
-// "searchstring",
-//  "example-filename.txt",
-// ]
+use std::process;
+
 fn main() {
-    let msg = "Should have been able to read the file";
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-    let contents = fs::read_to_string(file_path).expect(msg);
-    // Prints and returns the value of a given expression for quick and dirty debugging.
-    dbg!(query, file_path);
-    println!("searching for {}", query);
-    println!("in file {}", file_path);
-    println!("With text:\n{contents}");
+    let config = rust_book_cli::Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("searching for {}", config.query);
+    println!("in file {}", config.file_path);
+
+    if let Err(e) = rust_book_cli::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
+//     Config { query, file_path }
+// }
+
+// idiomatic refactor to remove clone
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args
+//         .get(1)
+//         .map(|s| s.to_owned())
+//         .unwrap_or_else(String::new);
+//     let file_path = args
+//         .get(2)
+//         .map(|s| s.to_owned())
+//         .unwrap_or_else(String::new);
+
+//     Config { query, file_path }
+// }
